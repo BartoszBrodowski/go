9f43a4277190a58d3f1cd05469cc3e876202f0cc
Bartosz Brodowski <bartolini10058@gmail.com>
2023-03-15 22:42:16 +0100

    Add loop to Collatz ver.1 and check for longest sequence

--- lab03/collatz.go
+++ lab03/collatz.go
@@ -12,8 +12,20 @@ func collatz(number int) []int {
 		}
 		sequence = append(sequence, number)
 	}
-	fmt.Println("Length of sequence: ", len(sequence))
 	return sequence
+}
+
+func biggestCollatzSequence() (int, int) {
+	max := len(collatz(10))
+	var max_number int 
+	for i := 10; i <= 100; i++ {
+		collatz_sequence := collatz(i)
+		if len(collatz_sequence) > max {
+			max = len(collatz_sequence)
+			max_number = i
+		}
+	}
+	return max_number, max
 }
 
 func main() {
@@ -22,4 +34,6 @@ func main() {
 	fmt.Scanln(&number)
 	collatz_sequence := collatz(number)
 	fmt.Println(collatz_sequence)
+	max_number, max_length := biggestCollatzSequence()
+	fmt.Println("Longest sequence comes out of number: ", max_number, " with length: ", max_length)
 }
