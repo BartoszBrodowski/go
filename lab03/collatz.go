5e01cd8078c49a27c2b6d5be9b19440c26d1e1dc
Bartosz Brodowski <bartolini10058@gmail.com>
2023-03-29 20:32:25 +0200

    Add exercises done

--- lab03/collatz.go
+++ lab03/collatz.go
@@ -15,10 +15,10 @@ func collatz(number int) []int {
 	return sequence
 }
 
-func biggestCollatzSequence() (int, int) {
+func biggestCollatzSequence(num1 int, num2 int) (int, int) {
 	max := len(collatz(10))
 	var max_number int 
-	for i := 10; i <= 100; i++ {
+	for i := num1; i <= num2; i++ {
 		collatz_sequence := collatz(i)
 		if len(collatz_sequence) > max {
 			max = len(collatz_sequence)
@@ -29,11 +29,11 @@ func biggestCollatzSequence() (int, int) {
 }
 
 func main() {
-	fmt.Print("Enter a number: ")
-	var number int
-	fmt.Scanln(&number)
-	collatz_sequence := collatz(number)
-	fmt.Println(collatz_sequence)
-	max_number, max_length := biggestCollatzSequence()
+	// fmt.Print("Enter a number: ")
+	// var number int
+	// fmt.Scanln(&number)
+	// collatz_sequence := collatz(number)
+	// fmt.Println(collatz_sequence)
+	max_number, max_length := biggestCollatzSequence(2000, 3000)
 	fmt.Println("Longest sequence comes out of number: ", max_number, " with length: ", max_length)
 }
--- lab04/main.go
+++ lab04/main.go
@@ -55,12 +55,57 @@ func matrixPrint(matrix [][]int) {
 	}
 }
 
-func main() {
-	sequence := vectorSum()
-	fmt.Println(sequence)
-	tablica1 := [][]int{{1, 2, 3}, {4, 5, 6}, {7, 8, 9}}
-	tablica2 := [][]int{{1, 2, 3}, {4, 5, 6}, {7, 8, 9}}
-	result := goodHadamardsFormula(tablica1, tablica2)
+func addTwoDimensionalArrayAndItsReverse(arr1 [][]int) {
+	arr2 := make([][]int, len(arr1))
+	result := make([][]int, len(arr1))
+	for i := range arr2 {
+		arr2[i] = make([]int, len(arr1[0]))
+		result[i] = make([]int, len(arr1[0]))
+	}
+
+	for i := 0; i < len(arr1); i++ {
+		for j := 0; j < len(arr1[0]); j++ {
+			arr2[i][j] = arr1[len(arr1) - 1 - i][len(arr1[0]) - 1 - j]
+		}
+	}
+
+	for i := 0; i < len(arr1); i++ {
+		for j := 0; j < len(arr1[0]); j++ {
+			result[i][j] = arr1[i][j] + arr2[i][j]
+		}
+	}
+
 	fmt.Println(result)
-	matrixPrint([][]int{{1, 2, 3}, {4, 5, 6}, {7, 8, 9}})
+}
+
+func multiplyMatrixes(matrix1 [][]int, matrix2 [][]int) {
+	if len(matrix1[0]) == len(matrix2) || len(matrix1) == len(matrix2[0]) {
+		result := make([][]int, len(matrix1))
+		for i := range result {
+			result[i] = make([]int, len(matrix2[0]))
+		}
+		for i := 0; i < len(matrix1); i++ {
+			for j := 0; j < len(matrix2[0]); j++ {
+				for k := 0; k < len(matrix2); k++ {
+					result[i][j] += matrix1[i][k] * matrix2[k][j]
+				}
+			}
+		}
+		fmt.Println(result)
+	} else {
+		fmt.Println("Matrixes can't be multiplied")
+	}
+}
+
+func main() {
+	// sequence := vectorSum()
+	// fmt.Println(sequence)
+	tablica1 := [][]int{{1, 2, 3}, {4, 5, 6}, {7, 8, 9}}
+	fmt.Println(tablica1[2][2])
+	tablica2 := [][]int{{1, 2, 3}, {4, 5, 6}, {7, 8, 9}}
+	// result := goodHadamardsFormula(tablica1, tablica2)
+	// fmt.Println(result)
+	// matrixPrint([][]int{{1, 2, 3}, {4, 5, 6}, {7, 8, 9}})
+	// addTwoDimensionalArrayAndItsReverse(tablica1)
+	multiplyMatrixes(tablica1, tablica2)
 }
